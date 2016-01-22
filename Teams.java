d34906b59fe176d7f0631d67d580c104133943d3
venki7474 <venkateshejjagiri@gmail.com>
2016-01-22 22:03:52 +0530

    Create Teams.java

---
+++ Teams.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main_della;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author Chotu
+ */
+public class teams {
+    public static 
+    ObservableList<String> teamsList = FXCollections.observableArrayList();
+    public static void addToList(String team){
+        
+    }
+    
+}
